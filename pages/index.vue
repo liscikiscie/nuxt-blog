1f0033047111dafea616b9ebc936dd44a0f293a1
notFromHere <jakubwasiljew@gmail.com>
2018-12-10 21:44:55 +0100

    admin section

--- pages/index.vue
+++ pages/index.vue
@@ -3,35 +3,18 @@
     <section class="intro">
       <h1>Get the latest tech news!</h1>
     </section>
-    <section class="featured-posts">
-      <PostPreview
-        id="1"
-        thumbnail="https://www.homage.sg/wp-content/uploads/2018/09/47724337_l-1080x675.jpg"
-        title="Hello there"
-        previewText="This is my first post!"
-      />
-      <PostPreview
-        id="2"
-        thumbnail="https://www.homage.sg/wp-content/uploads/2018/09/47724337_l-1080x675.jpg"
-        title="Hello there for the second time"
-        previewText="This is my first post!"
-      />
-      <PostPreview
-        id="3"
-        thumbnail="https://www.homage.sg/wp-content/uploads/2018/09/47724337_l-1080x675.jpg"
-        title="Hello there - Hi!"
-        previewText="This is my first post!"
-      />
-    </section>
+    <PostList/>
   </div>
 </template>
 
 <script>
 
-  import PostPreview from '../components/Posts/PostPreview';
+  import PostList from '@/components/Posts/PostList';
 
   export default {
-    components: {PostPreview}
+    components: {
+      PostList
+    }
   }
 </script>
 
@@ -65,15 +48,6 @@
     .intro h1 {
       font-size: 2rem;
     }
-  }
-
-  .featured-posts {
-    display: flex;
-    padding: 20px;
-    box-sizing: border-box;
-    flex-wrap: wrap;
-    align-items: center;
-    justify-content: center;
   }
 
 </style>
