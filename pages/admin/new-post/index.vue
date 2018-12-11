31c6a2f5aea4ae63ce0012dc202c63ea932b74e0
notFromHere <jakubwasiljew@gmail.com>
2018-12-11 14:02:37 +0100

    admin section basics

---
+++ pages/admin/new-post/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="admin-new-post-page">
+    <section class="new-post-form">
+      <AdminPostForm/>
+    </section>
+  </div>
+</template>
+
+<script>
+
+  import AdminPostForm from '../../../components/admin/AdminPostForm';
+
+  export default {
+    name: 'index',
+    components: {AdminPostForm}
+  }
+</script>
+
+<style scoped>
+
+</style>
